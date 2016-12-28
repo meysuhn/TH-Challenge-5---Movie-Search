04c2324c4af11da2bb9e6e793b87d1b6a1b54fbf
Chris Mason <meysuhn@gmail.com>
2016-12-28 14:37:46 +0000

    Trying to access JSON results

--- js/app.js
+++ js/app.js
@@ -1,5 +1,8 @@
+
+
 $(document).ready(function () {
-    $('#submit').click(function () {
+    $('#submit').click(function (evt) {// why evt and what does that mean? What is the significance of those particular three letters?
+        evt.preventDefault();
 
         var ombdAPI = "http://www.omdbapi.com";
 
@@ -7,24 +10,29 @@ $(document).ready(function () {
 
         var dataToGet = {
           s: searchData,
+          y: "year",
           type: "Movie",
           r: "json",
           callback: ""
         };
 
 
-        function displayMovies(movie) {
-            
+        function displayMovies(movie) { //the information received back from omdb is passed to this callback function, as we've named that parameter 'movie' that's what I'm telling the programme to store the received infromation in, i.e the info received is being stored in a variable called 'movie'. That information is an object.
+
         console.log("Callback has fired");
-            var movieHTML;
-            $.each(data.search, function (i, movie) {
-                movieHTML += '<li><div class="poster-wrap">';
-                movieHTML += '<img class="movie-poster" src="' + movie.poster + '"></div>';
-                movieHTML += '<span class="movie-title">"' + movie.title + '"</span>';
-                movieHTML += '<span class="movie-year">"' + movie.year + '"</span></li>';
-            });
-            $('#movies').html(movieHTML);
-            console.log(movieHTML);
+        console.log(movie);
+        console.log(typeof movie);
+        //$('#movies').html("is this working?");
+        var movieHTML = "";
+        console.log("Total Results: " + movie.totalResults); //total results is a key on the returned JSON object which I've named 'movie'
+
+        // the individual search results are objects stored in an array on the 'search' key, all stored inside the movie variable.
+        console.log(movie.search);
+        var storageArray = movie.search;
+        console.log(storageArray);
+
+        
+
         }
         $.getJSON(ombdAPI, dataToGet, displayMovies);
 
