4cdb02880126290a2b47b4c3249f0695cc01643e
Chris Mason <meysuhn@gmail.com>
2016-12-28 20:07:24 +0000

    Basic requirements met

--- js/app.js
+++ js/app.js
@@ -19,19 +19,41 @@ $(document).ready(function () {
 
         function displayMovies(movie) { //the information received back from omdb is passed to this callback function, as we've named that parameter 'movie' that's what I'm telling the programme to store the received infromation in, i.e the info received is being stored in a variable called 'movie'. That information is an object.
 
-        console.log("Callback has fired");
-        console.log(movie);
-        console.log(typeof movie);
-        //$('#movies').html("is this working?");
-        var movieHTML = "";
-        console.log("Total Results: " + movie.totalResults); //total results is a key on the returned JSON object which I've named 'movie'
+        if (movie.Response === "True") {
+            var movieHTML = "";
+            console.log("Total Results: " + movie.totalResults); //total results is a key on the returned JSON object which I've named 'movie'
 
-        // the individual search results are objects stored in an array on the 'search' key, all stored inside the movie variable.
-        console.log(movie.search);
-        var storageArray = movie.search;
-        console.log(storageArray);
+            // the individual search results are objects stored in an array on the 'Search' key, all stored inside the movie variable.
+            console.log(movie.Search);
 
-        
+
+                $.each(movie.Search, function (i, item) { //the array received from omdb is called 'search'. I access this by accessing the information inside the movie object.
+
+                movieHTML += '<li><div class="poster-wrap">';
+
+                // if poster image not present, display placehlder image, else  display poster image.
+                if (item.Poster === "N/A") {
+                    movieHTML += '<i class="material-icons poster-placeholder">crop_original</i></div>';
+
+                } else {
+                    movieHTML += '<img class="movie-poster" src="' + item.Poster + '"></div>';
+                }
+
+                movieHTML += '<span class="movie-title">"' + item.Title + '"</span>';
+                movieHTML += '<span class="movie-year">"' + item.Year + '"</span></li>';
+            });
+            $('#movies').html(movieHTML);
+
+        }
+
+        if (movie.Response === "False") {
+            console.log("FALSE");
+            var noMovies = "<li class='no-movies'><i class='material-icons icon-help'>help_outline</i>";
+            noMovies += "No movies found that match: " + searchData + "</li>";
+
+            $('#movies').html(noMovies);
+
+        }
 
         }
         $.getJSON(ombdAPI, dataToGet, displayMovies);
