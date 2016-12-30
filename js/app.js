ecab6771fbb4c03be7fa80ead0f1e44107e2c565
Chris Mason <meysuhn@gmail.com>
2016-12-30 00:34:20 +0000

    Completed draft

--- js/app.js
+++ js/app.js
@@ -1,117 +1,126 @@
 
+// TechDegree Challenge 5
+// Chris Mason 30/12/16
+// Checked on Chrome, Firefox, Safari, Opera
 
-$(document).ready(function () {
-    $('#submit').click(function (evt) {// why evt and what does that mean? What is the significance of those particular three letters?
-        evt.preventDefault();
+(function () {
 
-        var ombdAPI = "http://www.omdbapi.com";
+var allMovies = document.getElementById("movies"); //movies screen
+var movieDescriptionScreen = document.getElementById("descriptionPage"); //description screen
+var ombdAPI = "http://www.omdbapi.com";
 
-        var searchData = document.getElementById("search").value;
-        var yearData = document.getElementById("year").value;
-
-        var dataToGet = {
-          s: searchData, //switching code to take a t query may provide the more details response.
-          y: yearData,
-          plot: "full",
-          type: "Movie",
-          r: "json",
-          page: "1",
-          callback: ""
-        };
+allMovies.style.display = "block"; // show main screen on load.
+movieDescriptionScreen.style.display = "none"; // hide description screen on load.
 
 
-        function displayMovies(movie) { //the information received back from omdb is passed to this callback function, as we've named that parameter 'movie' that's what I'm telling the programme to store the received infromation in, i.e the info received is being stored in a variable called 'movie'. That information is an object.
+// function to take search and year data, send to OMDb api and display results on main page
+$('#submit').click(function (evt) {
+    evt.preventDefault();
 
-        if (movie.Response === "True") {
-            var movieHTML = "";
-            console.log("Total Results: " + movie.totalResults); //total results is a key on the returned JSON object which I've named 'movie'
+    if (allMovies.style.display === "none") { //show main page if new search is initiated from description page.
+        allMovies.style.display = "block";
+        movieDescriptionScreen.style.display = "none";
+    }
 
-            // the individual search results are objects stored in an array on the 'Search' key, all stored inside the movie variable.
-            console.log(movie.Search);
+    var searchData = document.getElementById("search").value; //get search input
+    var yearData = document.getElementById("year").value; //get year input
+
+    var dataToGet = { //data to send to api
+      s: searchData,
+      y: yearData,
+      plot: "full",
+      type: "Movie",
+      r: "json",
+      page: "1",
+      callback: ""
+    };
 
 
-                $.each(movie.Search, function (i, item) { //the array received from omdb is called 'search'. I access this by accessing the information inside the movie object.
-                //movieHTML += '<a href=http://www.imdb.com/title/' + item.imdbID +'>';
-                movieHTML += '<li><div class="poster-wrap">';
+    function displayMovies(movie) { //the information received back from omdb is passed to this callback function. As the parameter I've named 'movie' then that's what I'm telling the programme to store the received information in, i.e the info received is being stored in a variable called 'movie'. That information is an object.
 
-                // if poster image not present, display placehlder image, else  display poster image.
-                if (item.Poster === "N/A") {
-                    movieHTML += '<i class="material-icons poster-placeholder">crop_original</i></div>';
+    if (movie.Response === "True") {
+        var movieHTML = "";
+        console.log("Total Results: " + movie.totalResults); //total results is a key on the returned JSON object which I've named 'movie'
 
-                } else {
-                    movieHTML += '<img class="movie-poster" src="' + item.Poster + '"></div>';
-                }
+        // the individual search results are objects stored in an array on the 'Search' key, all stored inside the movie variable.
+        console.log(movie.Search);
 
-                movieHTML += '<span class="movie-title">"' + item.Title + '"</span>';
-                movieHTML += '<span class="movie-year">"' + item.Year + '"</span></li>';
 
-            });
-            $('#movies').html(movieHTML);
+            $.each(movie.Search, function (i, item) { //the array received from omdb is called 'search'. I access this by accessing the information inside the movie object.
 
-        }
+            // build up html with results
+            movieHTML += '<li><div class="poster-wrap">';
 
-        if (movie.Response === "False") {
-            var noMovies = "<li class='no-movies'><i class='material-icons icon-help'>help_outline</i>";
-            noMovies += "No movies found that match: " + searchData + "</li>";
+            // if poster image not present, display placehlder image, else  display poster image.
+            if (item.Poster === "N/A") {
+                movieHTML += '<i class="material-icons poster-placeholder">crop_original</i></div>';
 
-            $('#movies').html(noMovies);
+            } else {
+                movieHTML += '<img class="movie-poster" src="' + item.Poster + '"></div>';
+            }
 
-        }
+            movieHTML += '<span class="movie-title">"' + item.Title + '"</span>';
+            movieHTML += '<span class="movie-year">"' + item.Year + '"</span></li>';
 
-        }
-        $.getJSON(ombdAPI, dataToGet, displayMovies);
+        });
+        $('#movies').html(movieHTML); // replace previous content and insert new search results
 
-    });  // end submit click
+    }
 
-}); //end ready
+    // display error is no matches found
+    if (movie.Response === "False") {
+        var noMovies = "<li class='no-movies'><i class='material-icons icon-help'>help_outline</i>";
+        noMovies += "No movies found that match: " + searchData + "</li>";
 
+        $('#movies').html(noMovies);
+
+    }
+
+    }
+    $.getJSON(ombdAPI, dataToGet, displayMovies);
+
+});  // end submit click
+
+
+//function to get and display extra movie information on a new screen if user clicks on a movie result.
 $("#movies").click(function(event) {
-    var text = $(event.target).closest('li');
-    console.log(text);
-
-    var step1 = text[0];
+    var movieLi = $(event.target).closest('li');
+    var step1 = movieLi[0];
     var step2 = step1.children[1].innerText;
-    console.log(step2);
-
-    var ombdAPI = "http://www.omdbapi.com"; //this could be moved to global later.
-
-    // These later will need to be something like 'this' to catch the object clicked on. But for time being using dummy values
-    var searchData = step2;
-    //var yearData = document.getElementById("year").value;
+    var searchData = step2; // get the title of the movie
 
     var dataToGet = {
-      t: searchData, //switching code to take a t query may provide the more details response.
-      //y: yearData,
+      t: searchData, //access more descriptive info on requested movie using t
       plot: "full",
       type: "Movie",
       r: "json",
       callback: ""
     };
-    function movieDescription(description) {
-        console.log(description);
+    function movieDescription(description) { //callback function
         var descriptionHTML = "";
-
-        descriptionHTML += '<h3>' + description.Title + '</h3>';
-        descriptionHTML += '<h4>' + description.Year + '</h4>';
-        descriptionHTML += '<h4>IMDb Rating: ' + description.imdbRating + '</h4>';
+        descriptionHTML += '<div><h2><a href=http://www.imdb.com/title/' + description.imdbID +'>' + description.Title + " (" + description.Year + ")" + '</a></h2>';
+        descriptionHTML += '<h4>IMDb Rating: ' + description.imdbRating + '</h4></div>';
 
 
         //if poster image not present, display placehlder image, else  display poster image.
         if (description.Poster === "N/A") {
-            descriptionHTML += '<i class="material-icons poster-placeholder">crop_original</i></div>';
+            descriptionHTML += '<i class="material-icons poster-placeholder">crop_original</i>';
         } else {
             descriptionHTML += '<img class="movie-poster" src="' + description.Poster + '">';
         }
-        descriptionHTML += '<p>' + description.Plot + '</p>';
+        descriptionHTML += '<h3>Plot Synopsis: </h3><p>' + description.Plot + '</p>';
 
         descriptionHTML += '<span><a href=http://www.imdb.com/title/' + description.imdbID +'>See this movie on IMDb</a></span>';
 
-        $('#movies').html(descriptionHTML);
-
-
+        allMovies.style.display = "none"; // show all movies screen on load.
+        movieDescriptionScreen.style.display = "block"; // hide description screen on load.
+        $('#descriptionPage').html(descriptionHTML);
 
     }
     $.getJSON(ombdAPI, dataToGet, movieDescription);
 
 
 });
+
+
+}()); // end self executing function
